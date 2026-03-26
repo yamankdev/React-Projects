dc1f09094bccfc8a1549fbbefcba01831bb740b1
yamankdev <syamankumar.dev@gmail.com>
2026-03-26 21:23:52 +0530

    Added homepage in App.jsx and completed the styling.

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,121 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import PokemonCard from "./components/PokemonCard";
+
+const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
+const header = { type: "application/json" };
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  //   Pokemon Data Fetching Function
+  const fetchPokemon = async () => {
+    try {
+      const res1 = await fetch(API, header);
+      const data1 = await res1.json();
+      //   console.log(data1.results);
+
+      const allAPIs = data1.results.map(async (currData) => {
+        const res2 = await fetch(currData.url);
+        const data2 = await res2.json();
+        return data2;
+      });
+      //   console.log(detailedPokData);
+
+      const detailedRes = await Promise.all(allAPIs);
+      setPokemon(detailedRes);
+      setLoading(false);
+      //   console.log(detailedRes);
+    } catch (err) {
+      console.log(err.message);
+      setError(err);
+      setLoading(false);
+    }
+  };
+
+  //   Removing Side effects while fetching the data
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  //   Search Function
+  const searchPokemon = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase()),
+  );
+  //   console.log(searchPokemon);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>{error.message}</h1>
+      </div>
+    );
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <section className="container">
+        <header>
+          <h1>Let's Catch Pokemon</h1>
+        </header>
+        <div className="pokemon-search">
+          <input
+            type="text"
+            placeholder="Search Your Pokemon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
+          <ul className="cards">
+            {/* return <li key={currPokemon.id}>{currPokemon.name}</li>; */}
+            {/* fetchPokemon.map((currPokemon) => { */}
+            {searchPokemon.map((currPokemon) => {
+              return (
+                <PokemonCard key={currPokemon.id} pokemonData={currPokemon} />
+              );
+            })}
           </ul>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
